f8e7192811aba8371a2df01520a9224c24898db3
lara andrade <larastefannyas@gmail.com>
2024-10-10 17:11:25 -0300

    Commit

--- Estoque/src/classes_conexao/Produto.java
+++ Estoque/src/classes_conexao/Produto.java
@@ -1,15 +1,48 @@
 package classes_conexao;
 
 public class Produto {
-    String nome;
-    int quantidade;
-    double valor;
-    String dataEntrada;
+
+	private String nome;
+    private int quantidade;
+    private double valor;
+    private String dataEntrada;
 
     public Produto(String nome, int quantidade, double valor, String dataEntrada) {
         this.nome = nome;
         this.quantidade = quantidade;
         this.valor = valor;
+        this.dataEntrada = dataEntrada;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDataEntrada() {
+        return dataEntrada;
+    }
+
+    public void setDataEntrada(String dataEntrada) {
         this.dataEntrada = dataEntrada;
     }
 
